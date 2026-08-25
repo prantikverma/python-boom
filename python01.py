048a7260e28c7dda0fe0dafd0ea5cd294d47a23c
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-25 22:40:35 +0530

    Add files via upload

---
+++ python01.py
@@ -0,0 +1,10 @@
+print("prime is becoming great")
+
+name= "prime"
+# creating list of skills as python javascripts and sql
+skills=["python","javascipt","SQL"]
+# printing the first element 
+skills(0)
+ # printing the second element 
+skills(1)
+# counting the number of elements
