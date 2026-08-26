3b3a8b12de0130f82fe4a13e16db35b641713e88
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-26 23:49:50 +0530

    Add files via upload

---
+++ python04.py
@@ -0,0 +1,20 @@
+print("CONTAINER DATA TYPES IN PYTHON :\n")
+
+#list
+print("example of list")
+print([1,2,3,4,5,6,7,8])
+print("\n")
+
+#tuple
+print("example of tuple")
+print(("hi", "hello", "onichiva"))
+print("\n")
+
+#set
+print("example of set")
+print({1,2,3,4,5,6,7,8,9,10})
+print("\n")
+
+#dictionary
+print("example of dictionary")
+print({"name":"paras", "age":200, "gender":"male"})
