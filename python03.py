55f31b240ff1e8b641d72508bb3363cdd434c6f5
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-26 23:49:27 +0530

    Add files via upload

---
+++ python03.py
@@ -0,0 +1,43 @@
+print("""PYTHON SUPPORTS 3 CATEGORIES OF DATA TYPES
+
+      1. BASIC TYPES:
+
+                 A.INTEGER
+                 B.FLOAT
+                 C.COMPLEX
+                 D.BOOLEAN
+                 E.STRING
+
+      2. CONTAINER TYPES :
+
+                 A.LIST
+                 B.TUPLE
+                 C.SET
+                 DCLASS.DICTIONARY
+
+      3. USER DEFINED TYPE :
+
+                 A.CLASS\n""")
+
+print("BASIC TYPES DATATYPES")
+
+#INTEGER
+num=4
+print(num)
+print(1e308)
+print(1e309)#infinite
+
+#float
+print(1.2)
+print(1.7e308)
+print(1.7e309)#infinite
+
+#boolean
+print(True)
+print(False)
+
+#complex
+print(1+2j)
+
+#string
+print("hello future prantik")
