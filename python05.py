81100b5d4010147259bf9877e91f933340c31a67
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-27 00:52:49 +0530

    Add files via upload

---
+++ python05.py
@@ -0,0 +1,7 @@
+print("COMMENTS IN PYTHON.... a piece of code that is not executed by compiler or interpreted\n")
+
+print("objective .... to enhence code readability")
+print("applied using # symbol")
+print("there is no multiple line comment in python")
+
+#aaaj maine pda python comments ....HAhA
