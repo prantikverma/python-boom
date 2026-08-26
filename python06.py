9885e40c3a5fb39a34d847ea680c2657fe6ce1dc
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-27 00:53:19 +0530

    Add files via upload

---
+++ python06.py
@@ -0,0 +1,33 @@
+#variables in python
+print("VARIABLES IN PYTHON....fundamental concept")
+
+number1=20
+number2=30
+
+#number1 and number2 are variables 
+print("""in python no variable declaration needed....
+ie dynamic typing.... python interpreter automativcally recognises the type of data....
+no data type defining """)
+
+print(number1)
+
+number1=25
+
+number1=True
+print(number1)#here value will be True
+
+#data type can be changed easily on rewriting the values for given variables
+
+print("in python variable and datatype are not binded with each ....this is called dynamic binding")
+
+#multiple variable decalration
+a=1;b=2;c=3
+print(a,b,c)
+
+
+d,e,f=4,5,6
+print(d,e,f)
+
+
+g=h=i=10
+print(g,h,i)
