ea8b9ec5677bb456521437ad96fea38e5b989eb6
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-26 23:48:10 +0530

    Add files via upload

---
+++ python02.py
@@ -0,0 +1,14 @@
+print("print function and parameter\n")
+
+print("hello duniya walo\n")
+print(False,"\n")
+print(50000000000000000000000,"\n")
+print("india","pakistan","china","\n")
+print("india, pakistan, china, bhutan, srilanka\n")
+
+#sep parameter
+print("france","germany","poland", "\n" ,sep="?")
+
+#end parameter
+print("india is my ",end="")
+print("country")
