ca4c6194b2a0a1b70e8920fd2477f985def7d03a
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-27 09:07:17 +0530

    Add files via upload

---
+++ python08.py
@@ -0,0 +1,14 @@
+#input from user
+print("hiiii today we will take input from user")
+
+print("type your name: ")
+name=input()
+#bydefault string data type caause string is a universal format
+
+first_number=input("enter the first number:")
+second_number=input("enter the second number:")
+
+result= first_number + second_number
+print(result)
+#result will be concatenation of 2 numbers
+#we need to perform type inversion
