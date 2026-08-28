3d71606b54b57a358b1046f261927a7f6ec7907d
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-28 17:01:49 +0530

    Add files via upload

---
+++ python12.py
@@ -0,0 +1,15 @@
+# IF ELSE STATEMENT
+print("lets go for the if else statement today\n")
+print("""syntax :
+if(condition):
+    statement
+else:
+    statement
+\n""")
+email_inserted=input("email enter krr: ")
+pass_inserted=input("enter the password:")
+
+if (email_inserted=="prantikvermaparas@gmail.com" and pass_inserted=="1234"):
+    print("login successful")
+else:
+    print("try again")
