98a46102ac87f8fc5ef656c4093ea08c8a38f192
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-28 21:58:29 +0530

    Add files via upload

---
+++ python16.py
@@ -0,0 +1,23 @@
+#LOOPS OR ITERATIONS IN PYTHON
+print("loops in python for repetition")
+
+print("2 loops in python:\n" \
+"1. while loop\n" \
+"2. for loop\n")
+
+print("""syntax for while loop:
+
+while condition:
+    code
+
+\n""")
+
+#printing table using while loop
+print("printing table ")
+num=int(input("enter any number : "))
+i=1
+while i<=10:
+    print(num,"*",i,"=",num*i)
+    i=i+1
+
+print("\n WHILE LOOP DONE")
