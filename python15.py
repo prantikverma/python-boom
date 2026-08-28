c75daa5659d6b5eb3e5781abd07629260b8ebd2c
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-28 20:48:55 +0530

    Add files via upload

---
+++ python15.py
@@ -0,0 +1,26 @@
+#INDENTATION IN PYTHON
+print("\npython do not uses ';' and '{}'")
+
+print("""indentation used for:
+1. separate block of code 
+2. tells about termination of code statement\n""")
+
+print("""le me provide you example:
+if(name=='xyz')
+{   some code;
+    some code;
+}else
+{   some code;
+    some code;
+}""")
+
+#same code in python
+name=input("enter the name bro: ")
+if name=="xyz":
+    print("spme code under if block\n")
+    
+else:
+    print("some code under else block\n")
+    
+
+print("it was all about indentation to differentiate block of code")
