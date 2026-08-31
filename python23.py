0edd4bc9dfb701c6c29485bc6eb970635499f8ee
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-08-31 23:47:22 +0530

    Add files via upload
    creating strings in python

---
+++ python23.py
@@ -0,0 +1,24 @@
+#strings in python
+print("STRINGS IN PYTHON")
+print("in python speciafically , strings are a group of sequence of unicode characters")
+#ascii is for only english 
+#unicode  is 16 bit code for characters.... uncluding more languages 
+
+print("""
+1.creating strings 
+2.assessing strings
+3.adding chars to string
+4.edting strings
+5.deleting strings
+6.operations on strings
+7.string functions
+
+""")
+
+#creating strings
+fname="prantik"
+lname='verma'
+situation="it's raining outside"
+#triple quotes for mutliline strings
+
+
