6bcca28cbaecf2d182d8b2558a54efb089553cf7
PRANTIK VERMA <138207176+prantikverma@users.noreply.github.com>
2026-09-03 23:25:16 +0530

    Add files via upload

---
+++ python28.py
@@ -0,0 +1,39 @@
+#specialised functions in python
+
+print("""1. capitalise/title/upper/lower/swapcase""")
+
+c='kolkata'
+
+print(c.capitalize())
+#Kolkata
+
+print(c)
+#kolkata
+ 
+d= "it is raining"
+print(d.capitalize())
+#It is raining
+
+print(d.title())
+#It Is Raining
+
+print(d.upper())
+#IT IS RAINING 
+
+print(d.lower())
+#it is raining
+
+print(d.swapcase())
+
+print("2.count ")
+
+e="i sm prantik"
+print(e.count('i'))
+#2
+
+print(e.count('ik'))
+#1
+
+print(e.count("d"))
+#0 
+
